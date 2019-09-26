24a9e319caef9b02bb6a31e201912d807b984cbb
lsg_stone <42756919+Amador09@users.noreply.github.com>
2019-09-26 12:05:07 -0500

    01

---
+++ entornovirtual/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\amador\desktop\amdor\ventasdjango\entornovirtual\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
